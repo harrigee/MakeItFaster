457d18f3f21b0d27f46f7e0dd4cffe596517057c
Stefan Gregor <stefan.gregor@mantro.net>
2016-09-13 12:37:46 +0200

    initial commit

--- MakeItFaster/ViewController.swift
+++ MakeItFaster/ViewController.swift
@@ -10,14 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var fasterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
--- MakeItFasterTests/MakeItFasterTests.swift
+++ MakeItFasterTests/MakeItFasterTests.swift
@@ -21,16 +21,8 @@ class MakeItFasterTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testViewController() {
+        let vc:ViewController? = ViewController()
+        assert(vc != nil)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
